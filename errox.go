334dada08c4d59b34bdf8e758fd0141f018311e8
pillar <ymblender@gamil.com>
2021-07-18 12:28:59 +0800

    fix: add some doc

--- errox.go
+++ errox.go
@@ -6,8 +6,6 @@ import (
 	"runtime"
 )
 
-var Debug = false
-
 func wrapStack(err error) error {
 	_, file, line, ok := runtime.Caller(2)
 	if !ok {
@@ -16,16 +14,15 @@ func wrapStack(err error) error {
 	return fmt.Errorf("\n%s:%d : %w", file, line, err)
 }
 
-// Line Make stack info string when errox.Debug = true
-func Line() string {
+// Change debug bool, base is false
+var Debug = false
+
+// Create a error and add stack info string when errox.Debug = true
+func New(msg string) error {
 	if Debug {
-		_, file, line, ok := runtime.Caller(1)
-		if !ok {
-			return "[bug]WrapError runtime.Caller(2) Fail"
-		}
-		return fmt.Sprintf("\n%s:%d", file, line)
+		return wrapStack(errors.New(msg))
 	}
-	return ""
+	return errors.New(msg)
 }
 
 // Add stack info to error when errox.Debug = true
@@ -39,6 +36,7 @@ func Wrap(err error) error {
 	return err
 }
 
+// Same errox.Wrap, add format string
 func Wrapf(err error, format string, a ...interface{}) error {
 	if err == nil {
 		return nil
@@ -49,16 +47,22 @@ func Wrapf(err error, format string, a ...interface{}) error {
 	return err
 }
 
-func New(msg string) error {
-	if Debug {
-		return wrapStack(errors.New(msg))
-	}
-	return errors.New(msg)
-}
-
+// Add stack info and fmt.Errorf when errox.Debug = true
 func Errorf(format string, a ...interface{}) error {
 	if Debug {
 		return wrapStack(fmt.Errorf(format, a...))
 	}
 	return fmt.Errorf(format, a...)
+}
+
+// Make stack info string when errox.Debug = true
+func Line() string {
+	if Debug {
+		_, file, line, ok := runtime.Caller(1)
+		if !ok {
+			return "[bug]WrapError runtime.Caller(2) Fail"
+		}
+		return fmt.Sprintf("\n%s:%d", file, line)
+	}
+	return ""
 }
